586e06d0f0c4ae13a4974b9f92b02557cd9d62a0
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-13 17:51:44 -0500

    Section 4 Completed

--- Section_4/beerAPI/src/main/java/com/restful/brewery/beerAPI/client/BeerClientImpl.java
+++ Section_4/beerAPI/src/main/java/com/restful/brewery/beerAPI/client/BeerClientImpl.java
@@ -1,40 +1,82 @@
 package com.restful.brewery.beerAPI.client;
 
+import com.restful.brewery.beerAPI.config.WebClientProperties;
 import com.restful.brewery.beerAPI.model.BeerDto;
 import com.restful.brewery.beerAPI.model.BeerPagedList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
 import java.util.UUID;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final WebClient webClient;
     @Override
     public Mono<BeerDto> getBeerById(UUID id, Boolean showInventoryOnHand) {
-        return null;
+        return webClient.get().uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID)
+                .queryParamIfPresent("showInventoryOnHand", Optional.ofNullable(showInventoryOnHand))
+                .build(id))
+                .retrieve()
+                .bodyToMono(BeerDto.class);
     }
 
     @Override
     public Mono<BeerPagedList> listBeers(Integer pageNumber, Integer pageSize, String beerName, String beerStyle, Boolean showInventoryOnhand) {
-        return null;
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH)
+                        .queryParamIfPresent("pageNumber", Optional.ofNullable(pageNumber))
+                        .queryParamIfPresent("pageSize", Optional.ofNullable(pageSize))
+                        .queryParamIfPresent("beerName", Optional.ofNullable(beerName))
+                        .queryParamIfPresent("beerStyle", Optional.ofNullable(beerStyle))
+                        .queryParamIfPresent("showInventoryOnhand", Optional.ofNullable(showInventoryOnhand))
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(BeerPagedList.class);
     }
 
     @Override
-    public Mono<ResponseEntity> createBeer(BeerDto beerDto) {
-        return null;
+    public Mono<ResponseEntity<Void>> createBeer(BeerDto beerDto) {
+        return webClient.post()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH)
+                        .build())
+                .body(BodyInserters.fromValue(beerDto))
+                .retrieve()
+                .toBodilessEntity();
     }
 
     @Override
-    public Mono<ResponseEntity> updateBeer(BeerDto beerDto) {
-        return null;
+    public Mono<ResponseEntity<Void>> updateBeer(UUID beerId,BeerDto beerDto) {
+        return webClient.put()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID)
+                        .build(beerId))
+                .body(BodyInserters.fromValue(beerDto))
+                .retrieve()
+                .toBodilessEntity();
     }
 
     @Override
-    public Mono<ResponseEntity> deleteBeerById(UUID id) {
-        return null;
+    public Mono<ResponseEntity<Void>> deleteBeerById(UUID id) {
+        return webClient.delete()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_PATH_GET_BY_ID)
+                        .build(id))
+                .retrieve()
+                .toBodilessEntity();
     }
 
     @Override
     public Mono<BeerDto> getBeerByUPC(String upc) {
-        return null;
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(WebClientProperties.BEER_V1_UPC_PATH)
+                        .build(upc))
+                        .retrieve()
+                        .bodyToMono(BeerDto.class);
     }
 }
