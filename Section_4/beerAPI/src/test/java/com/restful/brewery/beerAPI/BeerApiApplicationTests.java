317e1c0cad156dfab6a132f9bd393c1ebd70c8e5
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-13 10:25:45 -0500

    Starting Brewery API project

---
+++ Section_4/beerAPI/src/test/java/com/restful/brewery/beerAPI/BeerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.brewery.beerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
